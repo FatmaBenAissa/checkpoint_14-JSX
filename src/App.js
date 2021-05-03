0e22865772eed76b1d5bed9e2eb246726d03eaa2
fatma <fatmabenaissa1@gmail.com>
2021-05-03 12:19:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import image from "./b.jpg";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className={"container"}>
+  
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}} >
+
+       <h1 className={"title red"}>Fatma Ben Aissa</h1>
+      <div className="div1">
+
+        <img src="/a.jpg" className="im1"></img>
+
+
+        <img src={image} className="im1"></img> 
+  
+      </div>
     </div>
+
+
+
+<video style={{width:"320", height:"240" }} controls>
+
+<source src="myVideo.mp4" type="video/mp4" ></source>
+
+</video>
+
+</div>
+
   );
 }
 
